28463b3baf365b058983c5419763f1e041151e42
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2024-11-18 17:31:41 +0100

    Setup the server

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("server listening on http://localhost:8080");
+});
